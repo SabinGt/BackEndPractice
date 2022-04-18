5645def56d9f5b91140a7b6b9cec8ba62f844f90
Sabin Gautam <sabingt0@gmail.com>
2022-04-18 16:09:00 +0545

    routing and controllers fixed

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express')
 const dotenv = require('dotenv');
 dotenv.config();
 const PORT = process.env.PORT
-const userRoute = require('./src/routes/userRoute')
+const userRoute = require('./src/routes/userRoute');
+const databaseConnection = require('./src/config/config');
 const app = express();
 
+//routing
 app.use('/Users',userRoute);
 
 app.listen(PORT , ()=>{
