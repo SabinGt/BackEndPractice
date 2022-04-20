4a3398b10ec994c768b13ecb291f830497d2535d
Sabin Gautam <sabingt0@gmail.com>
2022-04-20 17:54:22 +0545

    contoller for login and signup

---
+++ src/config/config.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import db from "../models/index.js";
+
+const Role = db.role;
+// const { default: mongoose } = require("mongoose");
+// const dotenv = require("dotenv");
+
+dotenv.config();
+// console.log(process.env.CONNECTION_STRING_ATLAS)
+export const databaseConnection = () => {
+  mongoose
+    .connect(process.env.CONNECTION_STRING_ATLAS)
+    .then(() => {
+      console.log("Database Connection has been made");
+      initial();
+    })
+    .catch((err) => {
+      console.log("err");
+    });
+};
+
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+        console.log("added `user` to roles collection");
+      });
+      new Role({
+        name: "admin",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+        console.log("added `admin` to roles collection");
+      });
+      new Role({
+        name: "moderator",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+        console.log("added `moderator` to roles collection");
+      });
+    }
+  });
+}
---
+++ src/middlewares/authJwt.js
@@ -0,0 +1,92 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+import db from "../models/index.js";
+const Role = db.role;
+const User = db.user;
+const secret = process.env.SECRET_KEY;
+
+verifyToken = (req, res, next) => {
+  let token = req.headers["x-access-token"];
+  if (!token) {
+    return res.status(403).send({ message: "No token found" });
+  }
+  jwt.verify(token, secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized!" });
+    }
+    req.userId = decoded.id;
+    next();
+  });
+};
+
+isAdmin = (req, res, next) => {
+  User.findById(req.userId).exec((err, user) => {
+    if (err) {
+      res.status(500).send({
+        message: err,
+      });
+      return;
+    }
+    Role.find(
+      {
+        _id: { $in: user.roles },
+      },
+      (err, roles) => {
+        if (err) {
+          res.status(500).send({
+            message: err,
+          });
+          return;
+        }
+        for (let i = 0; i < roles.length; i++) {
+          if (roles[i] == "name") {
+            next();
+            return;
+          }
+        }
+        res.status(403).send({ message: "Require Admin Role!" });
+        return;
+        
+      }
+    );
+  });
+};
+
+isModerator = (req, res, next)=>{
+    User.findById(req.userId).exec((err,user)=>{
+        if(err){
+            res.status(500).send({
+                message: err,
+              });
+            return
+        }
+        Role.find({
+            _id: {$in:user.roles}
+        },(err,roles)=>{
+            if(err){
+                res.status(500).send({
+                    message: err,
+                  });
+                  return;
+            }
+            for(let i=0; i<roles.length ;i++){
+                if(roles[i]=="moderator"){
+                    next();
+                    return;
+                }
+            }
+            res.status(403).send({ message: "Require Moderator Role!" });
+            return;
+        }
+        )
+    })
+}
+
+const authJwt = {
+    verifyToken,
+    isAdmin,
+    isModerator
+  };
+
+export default authJwt;
