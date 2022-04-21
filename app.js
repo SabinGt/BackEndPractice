160def892db835d1ec838258e8b49e5a0d469c3d
Sabin Gautam <sabingt0@gmail.com>
2022-04-21 09:57:16 +0545

    changes to auth routing

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-const express = require('express')
-const dotenv = require('dotenv');
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
-const PORT = process.env.PORT
-const userRoute = require('./src/routes/userRoute');
-const databaseConnection = require('./src/config/config');
+const PORT = process.env.PORT;
+import contactRoute from "./src/routes/contact.route.js";
+import authRoute from "./src/routes/auth.routh.js"
+import { databaseConnection } from "./src/config/config.js";
+
 const app = express();
 
-//routing
-app.use('/Users',userRoute);
+//middleware
+app.use(express.json());
 
-app.listen(PORT , ()=>{
-    console.log("sever is running on" + PORT)
-})
+//user auth route 
+app.use("/user/auth", authRoute)
+//user contact routing
+app.use("/user/contact", contactRoute);
+
+//database connection
+databaseConnection();
+
+app.listen(PORT, () => {
+  console.log("sever is running on " + PORT);
+});
